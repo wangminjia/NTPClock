946035eaaa99fdaf97d2733f70d8c616aea59945
Wang MinJia <wangminjia2001@gmail.com>
2018-10-07 19:47:45 +0800

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -7,7 +7,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
     sta_if.active(True)
-    sta_if.connect('Mozilla', '6539282828')
+    sta_if.connect('', '')
     while not sta_if.isconnected():
         pass
     print('network config:', sta_if.ifconfig())
